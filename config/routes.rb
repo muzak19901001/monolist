cd8f56f77f81a8b44d8bdfc0f9130568c00bb4f5
Tomonari.sato <muzak1001@outlook.jp>
2019-01-21 14:34:59 +0000

    rakuten api

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  
   resources :users, only: [:show, :new, :create]
+  
+  resources :items, only: [:new]
 end
