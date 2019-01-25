7250180a6b4ed7dcf84d826982d0635bd8b6a71e
Tomonari.sato <muzak1001@outlook.jp>
2019-01-25 13:55:04 +0000

    want

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   
   resources :users, only: [:show, :new, :create]
   
-  resources :items, only: [:new]
+  resources :items, only: [:new, :show]
+  resources :ownerships, only: [:create, :destroy]
+  
 end
