4bbd0e26f7c6850b9d6c2a862f72381fe59eb016
Tomonari.sato <muzak1001@outlook.jp>
2019-01-26 14:32:16 +0000

    ranking have

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'rankings/want', to: 'rankings#want'
+  get 'rankings/have', to: 'rankings#have'
+  
   
   resources :users, only: [:show, :new, :create]
   
